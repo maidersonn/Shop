3236e89eebd24979e20d6d776a3b6650cef10336
Maider Landaribar <maiderlandaribar@Maiders-MacBook-Pro.local>
2024-05-07 13:40:28 +0200

    unit tests: ArticleMapperTest

---
+++ src/test/java/com/maider/shop/controllers/mapper/ArticleMapperTest.java
@@ -0,0 +1,40 @@
+package com.maider.shop.controllers.mapper;
+
+import com.maider.shop.controllers.dto.ArticleCreationDTO;
+import com.maider.shop.controllers.dto.ArticleDTO;
+import com.maider.shop.domain.entities.Article;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ArticleMapperTest {
+    @Test
+    public void shouldGiveBackArticleDTO() {
+        ArticleMapper mapper = new ArticleMapper();
+        Article article = new Article("Trouser", 38, "cotton", "Batela", 10.5);
+
+        ArticleDTO articleDTO = mapper.toDto(article);
+
+        assertInstanceOf(ArticleDTO.class, articleDTO);
+        assertEquals("cottonTrouser", articleDTO.getName());
+        assertEquals(article.getBrand(), articleDTO.getBrand());
+        assertEquals(article.getSize_(), articleDTO.getSize());
+        assertEquals(article.getPrice(), articleDTO.getPrice());
+    }
+    @Test
+    public void shouldGiveBackArticle() {
+        ArticleMapper mapper = new ArticleMapper();
+        ArticleCreationDTO articleDTO = new ArticleCreationDTO("Trouser", "cotton", "Batela", 38, 10.5);
+
+        Article article = mapper.toArticle(articleDTO);
+
+        assertInstanceOf(Article.class, article);
+        assertEquals(articleDTO.getBrand(), article.getBrand());
+        assertEquals(articleDTO.getMaterial(), article.getMaterial());
+        assertEquals(articleDTO.getType(), article.getType());
+        assertEquals(articleDTO.getSize(), article.getSize_());
+        assertEquals(articleDTO.getPrice(), article.getPrice());
+    }
+
+}
